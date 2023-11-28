1cb821059e1649b2167a9b87545c0c89c2c1c0f6
Powsihan <powsipowsihan07@gmail.com>
2023-11-28 21:26:07 +0530

    css changes and category part some added

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "../App.css";
+import "../styles/Category.css";
 import Sidebar from "../layouts/Sidebar";
+
 function Category() {
   return (
     <Sidebar>
-        <div><h1>category</h1></div>
+      <div className="container">
+        <nav className="navbar mt-3 py-1 mb-3 rounded">
+          <div className="container-fluid d-flex align-items-center justify-content-center">
+            <span className="navbar-brand mb-0 h1 text-white">
+              Category Section
+            </span>
+          </div>
+        </nav>
+        <div className="container Category-FilterSection">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="row">
+          <div className="col Category-FilterSection"></div>
+          <div className="col Category-FilterSection"></div>
+        </div>
+      </div>
     </Sidebar>
   );
 }
