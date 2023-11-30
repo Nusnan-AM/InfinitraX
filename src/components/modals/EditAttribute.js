f406576c33927b4f5e8d29854c9e642fef3fa382
Nethma Kalpani <86551495+Kalpani1234@users.noreply.github.com>
2023-11-30 09:23:56 +0530

    Created separate components for modals

---
+++ src/components/modals/EditAttribute.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "../../App.css";
+import { Modal, Button } from "react-bootstrap";
+
+export default function EditAttribute(props) {
+  const { show, onHide } = props;
+
+  return (
+    <>
+      <Modal show={show} onHide={onHide}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Attribute</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <div className="modal-body text-white">
+              <div className="form-outline mb-4">
+                <label className="form-label" htmlFor="attribute">
+                  Attribute
+                </label>
+                <select
+                  id="selectEditAttribute-attribute"
+                  className="form-select"
+                >
+                  <option selected>--Select an Attribute--</option>
+                  <option>Color</option>
+                  <option>Storage</option>
+                  <option>Display</option>
+                </select>
+              </div>
+              <div className="form-outline mb-4">
+                <label className="form-label" htmlFor="value">
+                  Value
+                </label>
+                <input
+                  type="text"
+                  id="inputEditAttribute-value"
+                  className="form-control"
+                  required
+                />
+              </div>
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onHide}>
+            Close
+          </Button>
+          <Button variant="success">Edit</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
