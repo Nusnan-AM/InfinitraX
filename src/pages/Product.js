759042b8f9b060bb5c4c7c1162a8619c077101fe
Thilini Priyangika <85427990+Thilinipriyangika@users.noreply.github.com>
2023-11-30 23:02:01 +0530

    add filter section

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../layouts/Sidebar";
 import DataTable from "react-data-table-component";
-import { useState } from "react";
+import { Link } from "react-router-dom"; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+
 
 function Product() {
   const columns = [
@@ -32,19 +35,28 @@ function Product() {
     },
     {
       name: 'Action',
-      selector: row => row.action,
-      sortable: true
+      cell: row => (
+        <>
+          <Link to={`/view-product/${row.no}`}>
+            <FontAwesomeIcon icon={faEye} className="me-2" title="View" />
+          </Link>
+          <Link to={`/edit-product/${row.no}`}>
+            <FontAwesomeIcon icon={faEdit} className="me-2" title="Edit" />
+          </Link>
+          <FontAwesomeIcon icon={faTrash} className="text-danger" title="Delete" onClick={() => (row.no)} style={{ cursor: 'pointer' }} />
+        </>
+      )
     }
   ];
-  
-  const data=[
+
+  const data = [
     {
       no: 1,
       serialNo: 22,
       name: 'Thilini',
       category: 'laptops',
       brand: 'Asus',
-      action: ',.../'
+      action: ''
     },
     {
       no: 2,
@@ -52,28 +64,83 @@ function Product() {
       name: 'Nethma',
       category: 'keyboard',
       brand: 'hp',
-      action: '......'
-    },  
-  ]
-  const [records,setRecords] = useState(data);
+      action: ''
+    },
+    {
+      no: 3,
+      serialNo: 34,
+      name: 'Powsi',
+      category: 'mouse',
+      brand: 'asus',
+      action: ''
+    },
+    {
+      no: 4,
+      serialNo: 11,
+      name: 'Nusnan',
+      category: 'monitor',
+      brand: 'dell',
+      action: ''
+    }
+    
+  ];
 
-  function handleFilter(event){
-    const newData = data.filter(row=>{
-      return row.name.toLowerCase().includes(event.target.value.toLowerCase())
-    })
-    setRecords(newData)
+  const [records, setRecords] = useState(data);
+  const [categoryFilter, setCategoryFilter] = useState('');
+  const [brandFilter, setBrandFilter] = useState('');
+
+  function handleFilter(event) {
+    const { name, value } = event.target;
+
+    if (name === 'serialNo') {
+      const newData = data.filter(row => {
+        return String(row.serialNo).toLowerCase().includes(value.toLowerCase());
+      });
+      setRecords(newData);
+    } else if (name === 'category') {
+      setCategoryFilter(value);
+      applyFilters(value, brandFilter);
+    } else if (name === 'brand') {
+      setBrandFilter(value);
+      applyFilters(categoryFilter, value);
+    }
+  }
+
+  function applyFilters(category, brand) {
+    const newData = data.filter(row => {
+      const categoryMatch = category === '' || row.category.toLowerCase() === category.toLowerCase();
+      const brandMatch = brand === '' || row.brand.toLowerCase() === brand.toLowerCase();
+      return categoryMatch && brandMatch;
+    });
+    setRecords(newData);
   }
 
   return (
     <Sidebar>
       <div className='container mt-5'>
-        <div className="text-start"><input type="text" placeholder="Filter by SerialNo" onChange={handleFilter}></input></div>
-        <DataTable
-          columns = {columns}
-          data = {records}
-          fixedHeader
-          pagination >
-        </DataTable>
+        <div className="text-start ">
+          <input type="text" placeholder="Filter by SerialNo" name="serialNo" onChange={handleFilter} />
+          <select name="category" onChange={handleFilter}>
+            <option value="">Filter by Category</option>
+            
+            <option value="laptops">Laptops</option>
+            <option value="keyboard">Keyboard</option>
+          </select>
+          <select name="brand" onChange={handleFilter}>
+            <option value="">Filter by Brand</option>
+            
+            <option value="Asus">Asus</option>
+            <option value="hp">HP</option>
+          </select>
+        </div>
+        <div className='container mt-5'>
+          <div className='text-end'>
+            <Link to="/add-product">
+              <button className="btn btn-success ms-2">Add Product</button>
+            </Link>
+          </div>
+        </div>
+        <DataTable columns={columns} data={records} fixedHeader pagination />
       </div>
     </Sidebar>
   );
